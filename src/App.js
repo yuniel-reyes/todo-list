149264c25159c1bf8fe5761ae012c9ba96516904
yuniel <reyesdevpro@gmail.com>
2022-10-28 18:39:50 +0200

    Create App class and instantiate the UI class - the UI class will be the view, just as App will be the controller and Model will be the model

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import UI from './UI.js';
+
+// controller
+export default class App {
+    constructor() {
+
+        // console.log(Project.projects);
+        // instantiate the a UI object and pass...                      
+        this.view = new UI(); // Project.projects
+    }
+}
+
+
+
+
