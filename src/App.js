f0be9337ccbe4f651ddb59a71264dcab5c14de52
yuniel <reyesdevpro@gmail.com>
2022-10-28 18:55:50 +0200

    Create three default projects  - inbox  - today  - week

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import UI from './UI.js';
+import ToDos from './model.js';
+import Project from './Projects.js';
+import _Storage from './Storage.js';
 
 // controller
 export default class App {
     constructor() {
+        this.inbox_project = new Project('inbox');
+        this.today_project = new Project('today');
+        this.week_project = new Project('this_week');
 
         // console.log(Project.projects);
         // instantiate the a UI object and pass...                      
